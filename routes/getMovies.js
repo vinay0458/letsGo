112d1d713e3e338c79dc067b0f5728f32bdaec5d
CHITIMIREDDY <vc860h@att.com>
2017-08-20 19:59:50 -0700

    get call gemovies with only specific details - hiding _id and movieUrls

--- routes/getMovies.js
+++ routes/getMovies.js
@@ -5,10 +5,17 @@ const mongoose = require('mongoose');
 const moviesData = require('../models/movies');
 
 // READ
+/*** Trails
+   * moviesData.find(function(err,data){ - To show all details available
+   * moviesData.find({}, 'moviename' ,function(err,data){  - To show only moviename
+   * var columnsToShow = 'moviename movieImage language isBigMovie rating MovieDetails'; - To get data needed
+   * To hide -id --> var columnsToShow = {'_id': 0, 'moviename': 1, 'movieImage':1, 'language': 1, 'isBigMovie': 1, 'rating': 1, 'MovieDetails': 1};
+   */
 
+var columnsToShow = {'_id': 0, 'moviename': 1, 'movieImage':1, 'language': 1, 'isBigMovie': 1, 'rating': 1, 'MovieDetails': 1};
 router.get('/', (req, res) => {
   console.log("get movies data");
-  moviesData.find(function(err,data){
+  moviesData.find({}, columnsToShow ,function(err,data){
   	console.log('data'+data);
   	res.send(data);
   });
